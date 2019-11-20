bd0b934285782dd610d465032abe4b2230ed532b
naimkim <naimkim2000@naver.com>
2019-11-20 23:11:31 +0900

    11_너진짜 서운하다

--- offerCards.c
+++ offerCards.c
@@ -1,43 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-//2장씩 초기카드 배부 한장씩 순서대 
-int main(int argc, char *argv[]) {
-	int i,j;
-	int pullCard;
-	int cardhold;
-	int n_user;
+#include <time.h>
+#include "mixCardtray.c" 
+void pullCard(void) {
 	int x;
+	int N_CARD;
+	
+	srand((unsigned)time(NULL));
+	x=rand()%N_CARD;
+	
+	return 0;
+}
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//2장씩 초기카드 배부 한장씩 순서대로 
+int offerCards(int argc, char *argv[]) {
+	int i,j;
+	int n_user=5;
+	int cardhold[n_user][2];
+		
 
-	//1. give one card for each players and dealer
-	for (i=0;i<n_user;i++)
-	{
-		cardhold[i][0] = pullCard();//카드 트레이에서 하나씩 꺼내는 함  수 
-		x=cardhold[0];
-		printf("->dealer's first card: %i\n ->player[i]'s first card: %d\n",x,pullCard);
-	}
-	//2. 딜러랑 플레이어한테 두번째 카드주기 
-	for (j=0;j<n_user;j++)
-	{
+	//1. give two card for each players 한장씩 따로따로 줘야되고 딜러-나-플레이어 순이다 
+	for (i=0;i<n_user;i++){
 		cardhold[i][1] = pullCard();
 		
-		printf("->dealer's second card:%d\n ->player[i]'s second card:%d\n",pullCard);
-	}
-	//3. give one card for me
-	{
+		for (j=0;j<n_user;j++){
+			cardhold[i][0] = pullCard();
+			break;
+		}//2. give two card for the operator
 		cardhold[n_user][0] = pullCard();
+	}
+	cardhold[n_user][1] = pullCard();
 	
-		printf("->your first card:%d\n",pullCard);
-	}
-
-	//4. 나한테 두번째 카드주기
-	{ 
-		cardhold[n_user][1] = pullCard();
-		
-		printf("->your second card:%d\n",pullCard);
-	}
-		 
+	
+	
+	
 	return 0;
 }
