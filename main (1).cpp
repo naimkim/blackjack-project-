f364dd8a99731414f435258e8a1a5cc6fb02d9b9
naimkim <naimkim2000@naver.com>
2019-11-21 16:20:54 +0900

    17_ican'tdo

--- main (1).cpp
+++ main (1).cpp
@@ -15,6 +15,7 @@
 
 #define N_MIN_ENDCARD		30
 
+#include "configUser.c"
 #include "mixCardtray.c"
 #include "betDollar.c"
 #include "offerCards.c"
@@ -24,6 +25,7 @@
 #include "getAction.c"
 #include "checkResult.c"
 #include "checkWinner.c"
+#include "printCard.c"
  
 
 
@@ -42,6 +44,7 @@ int cardhold[N_MAX_USER+1][N_MAX_CARDHOLD];	//cards that currently the players h
 int cardSum[N_MAX_USER];					//sum of the cards
 int bet[N_MAX_USER];						//current betting 
 int gameEnd = 0; 							//game end flag
+int ROUND=1;
 
 //some utility functions
 
@@ -69,19 +72,34 @@ int getCardNum(int cardnum) {
 
 //print the card information (e.g. DiaA)
 void printCard(int cardnum) {
-
+	int pattern;
+	int HART, DIA, SPADE, CLUB;
+	 switch(pattern){
+ 		 case 0 :
+  		 printf("HART");
+  		 break;
+ 		 case 1 :
+  		 printf("DIA");
+  		 break;
+  		case 2 :
+  		 printf("SPADE");
+   		break;
+ 		 case 3 :
+   		printf("CLUB");
+   		break;
+		 }
 	
 };
 
 
 //card array controllers -------------------------------
 
-//mix the card sets and put in the array
+//mix the card sets and put in the array-----------END
 int mixCardTray(void) {
 	
 }
 
-//get one card from the tray
+//get one card from the tray------------END
 int pullCard(void) {
 	
 }
@@ -102,24 +120,7 @@ int betDollar(void) {
 }
 
 
-//offering initial 2 cards
-void offerCards(void) {
-	int i;
-	//1. give two card for each players
-	for (i=0;i<n_user;i++)
-	{
-		
-		cardhold[i][0] = pullCard();
-		cardhold[i][1] = pullCard();
-		
-		printf("dealer:x\n player[i]:%d\n",pullCard);
-	}
-	//2. give two card for the operator
-	cardhold[n_user][0] = pullCard();
-	cardhold[n_user][1] = pullCard();
-	
-	return;
-}
+
 
 //print initial card status
 void printCardInitialStatus(void) {
@@ -159,65 +160,29 @@ int checkWinner() {
 
 
 int main(int argc, char *argv[]) {
-	int roundIndex = 0;
+	int cardIndex = 0;//사용한 카드 갯수  
 	int max_user=5;
 	int i;
+	int round=1; 
 	
 	srand((unsigned)time(NULL));
 	
 	//set the number of players
-	configUser(
-	while(1){
-		
-	printf("HOW MANY PLAYERS WILL PLAY THIS GAME?(max is 5) : ");
-	scanf("%d",&n_user);
-		
-	if (n_user<0){
-		printf("no\n");
-		continue;
-	}
-	else if(n_user>5){
-		printf("too many!\n");
-		continue;
-	}
-	else{
-		printf("-->card is mixed and put into the tray");
-		break;
-	}
-	
-	);
+	configUser();
 	//Game initialization --------
 	//1. players' dollar
-	N_DOLLAR(
-	
-	);
+	N_DOLLAR=$50;
 	//2. card tray
-	mixCardTray(
-	
-	);
+	mixCardTray();
 
 
 
 	//Game start --------
 	do {
+		printf("---------ROUND %d(cardIndex:%d)--------",round,cardIndex);
 		
-		betDollar(
-		int player [n_user];
-		printf("\n------BETTING STEP--------");
-		
-		printf("\n-->input your betting dollar(total:$%d) : ",N_DOLLAR);
-		scanf("%d",&dollar);
-		if(dollar>50)
-		printf("you have only $50.")
-		else
-		printf("->your betting(total:$%d) : %d",N_DOLLAR,dollar);
-		for(player=1;player<n_user;player++);
-		printf("\n-->player[] bets $%d",);
-		);
-		offerCards(
-		
-		); //1. give cards to all the players
-		
+		betDollar();
+		offerCards(); //1. give cards to all the players
 		printCardInitialStatus();
 		printf("\n------------------ GAME start --------------------------\n");
 		
@@ -228,28 +193,29 @@ int main(int argc, char *argv[]) {
 			while () //do until the player dies or player says stop
 			{
 				//print current card status printUserCardStatus();
-				printf("card : %s%d",)
+				printf("--------------CARD OFFERING-----------");
+				printCard();
 				//check the card status ::: calcStepResult()
 				
 				//GO? STOP? ::: getAction()
-				printf(":::ACTION? (0 - GO, others - STOP)");
-				others=getIntegerInput;
+				getAction();
 				//check if the turn ends or not
+				
 			}
-			roundIndex++;
+			
 			
 			
 		}
 		
 		//result
-		checkResult(
-		if
+		printf("------------------ROUND %d result----------",round);
+		checkResult();
 		
-		
-		);임
+		round++;
+
 	} while (gameEnd == 0);
 	
-	checkWinner();//함수니까 이렇게 쓰기만 해도 실행된다고 황연수가 말했음 그러니까 맞을거
+	checkWinner();
 	
 	
 	return 0;
